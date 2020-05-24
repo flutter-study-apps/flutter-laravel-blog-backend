c7b9e6bf07de729c5ffe4e19429e3465258e6815
ialexies <ialexies@gmail.com>
2020-05-24 22:20:52 +0800

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+
+
+
+<div id="section-header">
+   <h1> @yield('section-header') </h1>
+</div>
+
+<hr> 
+
+<div id="section-content">
+    @yield('section-content')
+</div>
+
+<hr>
+
+<div id="section-footer">
+    @yield('section-footer')
+</div>
